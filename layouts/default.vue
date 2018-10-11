0f86af81812d92b7a5201cdd0178e020c73c6cec
Vasit Juntong <be.juntong@gmail.com>
2018-10-11 19:56:07 +0700

    hide footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,11 @@
     <div class="container mx-auto px-4 h-full">
       <nuxt/>
     </div>
-    <footer class="w-full flex container mx-auto px-4 py-10">
-      <span>
-        <font-awesome-icon :icon="['fa', 'copyright']" /> Vasit Juntong 2018.
-      </span>
-    </footer>
+    <!--<footer class="w-full flex container mx-auto px-4 py-10">-->
+      <!--<span>-->
+        <!--<font-awesome-icon :icon="['fa', 'copyright']" /> Vasit Juntong 2018.-->
+      <!--</span>-->
+    <!--</footer>-->
   </div>
 </template>
 
